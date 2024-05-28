c6f6d65c1e200e709c8b3a315771d78e966c5a52
Nursulton Malikov <my.nursultanakount@gmail.com>
2024-05-28 12:58:45 +0500

    RFID

--- ESP32-noCam/src/main.cpp
+++ ESP32-noCam/src/main.cpp
@@ -6,6 +6,8 @@
 #include <Adafruit_AHTX0.h>
 #include "ThingSpeak.h"
 #include <WiFi.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 // MQTT Broker
 const char *mqtt_broker = "51.20.105.27"; //
@@ -19,17 +21,28 @@ const int mqtt_port = 1884;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-//thingspeak
-WiFiClient  client1;
+// thingspeak
+WiFiClient client1;
 
 unsigned long myChannelNumber = 2;
-const char * myWriteAPIKey = "9X3LFQDD0RDACX82";
+const char *myWriteAPIKey = "9X3LFQDD0RDACX82";
 
 // LCD
 LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // AHT10
 Adafruit_AHTX0 aht;
+
+// rfid
+#define SS_PIN 5
+#define RST_PIN 4
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key;
+
+// Init array that will store new NUID
+byte nuidPICC[4];
+String card = "";
 
 //===========PINS==================
 #define relayPIN 25
@@ -46,6 +59,71 @@ bool lcdFlag = 0;
 unsigned long ttimeLcd;
 unsigned long lastTime = 110000;
 
+void printDec(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    card += String(buffer[i], HEX);
+  }
+  Serial.println(card);
+  if (card.equals("e0c486e"))
+  {
+    doorFlag = 1;
+    ttime = millis();
+    digitalWrite(relayPIN, 1);
+  }
+}
+
+void rfid_read()
+{
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (rfid.PICC_IsNewCardPresent())
+  {
+
+    // Verify if the NUID has been readed
+    if (rfid.PICC_ReadCardSerial())
+    {
+
+      Serial.print(F("PICC type: "));
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // Check is the PICC of Classic MIFARE type
+      if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+          piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+          piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+      {
+        Serial.println(F("Your tag is not of type MIFARE Classic."));
+        return;
+      }
+
+      // if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+      //   rfid.uid.uidByte[1] != nuidPICC[1] ||
+      //   rfid.uid.uidByte[2] != nuidPICC[2] ||
+      //   rfid.uid.uidByte[3] != nuidPICC[3] )
+
+      Serial.println(F("card has been detected."));
+
+      // Store NUID into nuidPICC array
+      for (byte i = 0; i < 4; i++)
+      {
+        nuidPICC[i] = rfid.uid.uidByte[i];
+      }
+
+      Serial.println(F("The NUID tag is:"));
+      Serial.print(F("In dec: "));
+      printDec(rfid.uid.uidByte, rfid.uid.size);
+      Serial.println();
+      card = "";
+
+      // Halt PICC
+      rfid.PICC_HaltA();
+
+      // Stop encryption on PCD
+      rfid.PCD_StopCrypto1();
+    }
+  }
+}
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
@@ -168,8 +246,8 @@ void setup() //================================== SETUP ========================
 {
   Serial.begin(115200);
   delay(10);
-  
-  ThingSpeak.begin(client1);  // Initialize ThingSpeak
+
+  ThingSpeak.begin(client1); // Initialize ThingSpeak
 
   // initialize the lcd
   lcd.init();
@@ -213,11 +291,22 @@ void setup() //================================== SETUP ========================
   client.subscribe(subtopic2);
 
   // initialize aht10
-  if (! aht.begin()) {
+  if (!aht.begin())
+  {
     Serial.println("Could not find AHT? Check wiring");
-    while (1) delay(10);
+    while (1)
+      delay(10);
   }
   Serial.println("AHT10 or AHT20 found");
+
+  // initialize rfid
+  SPI.begin();     // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
 
   pinMode(relayPIN, OUTPUT);
   pinMode(buttonPin, INPUT);
